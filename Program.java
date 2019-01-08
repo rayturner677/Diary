7ab4dd85dd3bb8fa3761fac9706b20fb498dd684
Ray Turner <rturner@basecampcodingacademy.org>
2019-01-08 13:42:07 -0600

    Changed the main method to add multiline entries that is entered when the user types "end".
    Co-author-by: John <jmorgan@basecampcodingacademy.org>
    
    Co-author-by: Daniel <dpeterson@basecampcodingacademy.org>

--- Program.java
+++ Program.java
@@ -1,13 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Program {
     static Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Welcome to your journal\nUser: ");
+        System.out.print("Welcome to your journal\nUser: ");
         String user = reader.nextLine();
-        Dairy diary = new Dairy(user);
+        Dairy diary = new Dairy(user.trim());
 
-        System.out.println(diary.madeFile());
+        System.out.println("Insert your Entry: ");
+        ArrayList<String> entries = new ArrayList<String>();
+
+        while (true ){
+           String line =  reader.nextLine();
+           entries.add(line);
+            if (line.equals("end")){
+                entries.remove(entries.size()-1);
+                diary.insertEntry(entries);
+                break;
+            }
+
+
+
+        }
+        reader.close();
+//        Arrays.copyOfRange(entries, 0, entries.length - 2)
+
+
+
+
     }
 }
